4bf036e87ecae4bb095d779c071ddbfe91cbe137
marc <marcjensenvirklund@gmail.com>
2023-03-14 11:34:58 +0100

    initial setup

---
+++ testASPDocker/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace testASPDocker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
